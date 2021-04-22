9893966a3a8fce7271dc88eae357d3c2bf209a22
alexandrucurcubat <alexandrucurcubat@gmail.com>
2021-04-23 02:40:15 +0300

    remove api glbal prefix

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  app.enableCors({ origin: process.env.ALLOW_CORS.split(',') });
-  app.useGlobalPipes(new ValidationPipe());
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Ace of Bids')
     .setDescription('Ace of Bids API description')
@@ -17,6 +14,8 @@ async function bootstrap() {
     .build();
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, swaggerDocument);
+  app.enableCors({ origin: process.env.ALLOW_CORS.split(',') });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
